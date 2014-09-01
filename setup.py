b1d7a71961565885429cbef39a4d5a63644b8b23
Felipe Blassioli <felipeblassioli@gmail.com>
2014-09-01 16:04:34 -0300

    Missing import render_template

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 
 setup(
     name='Flask-SimpleRest',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/felipeblassioli/flask_rest',
     author='Felipe Blassioli',
     author_email='felipeblassioli@gmail.com',
